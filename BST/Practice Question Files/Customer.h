b794c3ae9c5ee361e844871094c40c9908f4d9ea
Oscar Olazabal <oscarolazabal@gmail.com>
2018-01-14 03:10:52 -0800

    update practice question files

--- BST/Practice Question Files/Customer.h
+++ BST/Practice Question Files/Customer.h
@@ -1,4 +1,9 @@
-#pragma once
+// Customer Class
+// Email Oscar Olazabal at omolazabal@csu.fullerton.edu if you have any
+// questions or concerns about this file.
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
 
 #include <iostream>
 #include <string>
@@ -6,64 +11,42 @@ using namespace std;
 
 class Customer {
 public:
-	Customer(const string name = "Tuffy", const string licensePlate = "1111111", bool premiumWash = false) {
-		this->name = name;
-		this->licensePlate = licensePlate;
-		this->premiumWash = premiumWash;
-	}
+    Customer(const string name = "Tuffy", const string license_plate = "1111111", bool premium_wash = false) {
+        this->name = name;
+        this->license_plate = license_plate;
+        this->premium_wash = premium_wash;
+    }
 
-	~Customer() {}
+    ~Customer() {}
 
-	void setName(const string name) {
-		this->name = name;
-	}
+    void set_name(const string name) { this->name = name; }
+    void set_license_plate(const string license_plate) { this->license_plate = license_plate; }
+    void set_premium_wash(bool premium_wash) { this->premium_wash = premium_wash; }
 
-	void setLicensePlate(const string licensePlate) {
-		this->licensePlate = licensePlate;
-	}
+    string get_name() const { return name; }
+    string get_license_plate() const { return license_plate; }
+    bool get_premium_wash() const { return premium_wash; }
 
-	void setPremiumWash(bool premiumWash) {
-		this->premiumWash = premiumWash;
-	}
+    bool operator>(const Customer & right) const { return (name > right.name); }
+    bool operator==(const Customer & right) const { return (name == right.name); }
+    bool operator<(const Customer & right) const { return (name < right.name); }
 
-	string getName() const {
-		return name;
-	}
-
-	string getLicensePlate() const {
-		return licensePlate;
-	}
-
-	bool getPremiumWash() const {
-		return premiumWash;
-	}
-
-	bool operator>(const Customer & right) const {
-		return(licensePlate > right.licensePlate);
-	}
-
-	bool operator==(const Customer & right) const {
-		return(licensePlate == right.licensePlate);
-	}
-
-	bool operator<(const Customer & right) const {
-		return(licensePlate < right.licensePlate);
-	}
-
-	friend ostream & operator<<(ostream & out, const Customer customer) {
-		out << "Name: " << customer.getName() << endl;
-		out << "License Plate Number: " << customer.getLicensePlate() << endl;
-		out << "Premium Wash: ";
-		if (customer.getPremiumWash())
-			out << "Yes\n";
-		else
-			out << "No\n";
-		return out;
-	}
+    friend ostream & operator<<(ostream & out, const Customer customer) {
+        out << "Name: " << customer.get_name() << endl;
+        out << "License Plate Number: " << customer.get_license_plate() << endl;
+        out << "Premium Wash: ";
+        if (customer.get_premium_wash())
+            out << "Yes\n";
+        else
+            out << "No\n";
+        return out;
+    }
 
 private:
-	string name;
-	string licensePlate;
-	bool premiumWash;
+    string name;
+    string license_plate;
+    bool premium_wash;
 
 };
+
+#endif
